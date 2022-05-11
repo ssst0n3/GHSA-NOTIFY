242445249911a4ddfaad5c834056b167eb08aba7
Lei Wang <ssst0n3@gmail.com>
2022-05-11 12:56:02 +0800

    git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
-set -ex
-env
-mkdir -p repo
-git clone ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY} repo
-cd repo
+#set -ex
+#env
+#mkdir -p repo
+#git clone ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY} repo
+#cd repo
 
 /notifier
 ls -lah output
